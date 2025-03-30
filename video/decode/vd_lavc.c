482bbf5156032c6cb210cb0c400defdeaf60f243
ZXEducation <developer@zhixing.co>
2025-03-30 15:35:23 +0800

    判断解码队列长度

--- video/decode/vd_lavc.c
+++ video/decode/vd_lavc.c
@@ -1291,7 +1291,7 @@ static int receive_frame(struct mp_filter *vd, struct mp_frame *out_frame) {
   }
 
   MP_INFO(vd, "QueueSize %d.\n", decoded_queue_size);
-  if (decoded_queue_size > 60) {
+  if (decoded_queue_size > 120) {
     can_track = true;
   } else if (decoded_queue_size < 30) {
     can_track = false;
